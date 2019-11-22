41dd9b60aa27d2731ef638b5fa311b05430b39a5
fuzzy <17826806525@163.com>
2019-11-22 14:17:20 +0800

    11.22

--- pages/add-input/add-input.vue
+++ pages/add-input/add-input.vue
@@ -1,11 +1,15 @@
 <template>
 	<view>
-		
+		<uni-nav-bar left-icon="back" left-text="返回" right-text="菜单" title="标题"></uni-nav-bar>
 	</view>
 </template>
 
 <script>
+	import uniNavBar from "@/components/uni-ui/uni-nav-bar/uni-nav-bar.vue"
 	export default {
+		components:{
+			uniNavBar
+		},
 		data() {
 			return {
 				
